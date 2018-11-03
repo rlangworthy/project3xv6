4b1593e051bd5810fe161448bbc51b2069e9002a
Robert Langworthy <robertlangworthy@dhcp81.merusouth125.iit.edu>
2018-11-03 15:03:49 -0500

    p2v

--- sysproc.c
+++ sysproc.c
@@ -21,7 +21,9 @@ sys_uv2p(void){
   pte_t *pte;
   
   //pgdir = myproc()->pgdir;
-  pgdir = (pde_t*)(P2V(mycpu()->ts.cr3));
+  pushcli();
+  pgdir = (pde_t*)(mycpu()->ts.cr3);
+  popcli();
 
   cprintf("page directory base is: %p\n",pgdir);
   pde = &pgdir[PDX(vaddr)];
