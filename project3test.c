743ee128c5907145a0125b33395cfc42a3e600b9
Robert Langworthy <robertlangworthy@Roberts-MacBook-Pro.local>
2018-11-03 20:05:49 -0500

    did stuff

--- project3test.c
+++ project3test.c
@@ -20,14 +20,14 @@ int main(int argc, char *argv[]){
   printf(0, "%d %d\n", uv2p((char*)&argv), uv2p((char*)(&argc)));
 
   printf(0, "Test 2: dynamically allocated variables\n");
-  char *mem1 = (char*)malloc(5000);
+  char *mem1 = (char*)malloc(10);
   char *mem2 = (char*)malloc(10);
   
   if(uv2p(mem2)-uv2p(mem1) == 10)
   	printf(0, "Test 2 Success\n");
   else
   	printf(0, "Test 2 Fail\n");
-  printf(0,"%d %d\n", uv2p(mem1), uv2p(mem2));
+  printf(0,"%d %d\n", uv2p((char*)*mem1), uv2p((char*)*mem2));
 
 
   exit();
