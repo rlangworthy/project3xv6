7f6783ec617cf52792d3217da95e06b601cd0a53
Robert Langworthy <robertlangworthy@Roberts-MacBook-Pro.local>
2018-11-03 19:36:29 -0500

    did stuff

--- project3test.c
+++ project3test.c
@@ -12,16 +12,23 @@
 
 int main(int argc, char *argv[]){
 
-//  char *str1 = "test1";
-  //char *str2 = "test2";
+  printf(0, "Test 1: argc and argv\n");
+  if(uv2p((char*)&argv)-uv2p((char*)(&argc)) = 4)
+  	printf(0, "Test 1 success\n");
+  else
+	printf(0,"Test 1 fail\n");
+  
+  printf(0, "Test 2: statically allocated variables\n")
+  char *str1 = "test1";
+  char *str2 = "test2";
+  printf(0, "%d %d\n", uv2p(str1), uv2p(str2));
 
-//  char *mem1 = (char*)malloc(10);
-  //char *mem2 = (char*)malloc(10);
-  //uv2p(test3);
+  char *mem1 = (char*)malloc(10);
+  char *mem2 = (char*)malloc(10);
+  
 
   printf(0,"%d\n", uv2p((char*)(-10)));
-  printf(0,"%d\n", uv2p((char*)(&argc)));
-  printf(0,"%d\n", uv2p((char*)&argv));
+
 
   exit();
 
